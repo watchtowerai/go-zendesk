37eb5a4aafaff44501c56e58cc7445c207c55d57
Suhail Ansari <suhail.ism@gmail.com>
2025-09-30 15:21:02 -0700

    add APIs to redact chat comment and chat comment attachments (#10)

---
+++ zendesk/chat.go
@@ -0,0 +1,68 @@
+package zendesk
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+)
+
+type RedactChatCommentRequest struct {
+	TicketID  int64  `json:"id"`
+	MessageID string `json:"message_id"`
+	Text      string `json:"text"`
+}
+
+type RedactChatCommentAttachmentRequest struct {
+	TicketID   int64    `json:"id"`
+	MessageIDs []string `json:"message_ids"`
+}
+
+type RedactChatResponse struct {
+	ChatEvent struct {
+		ID    int64  `json:"id"`
+		Type  string `json:"type"`
+		Value struct {
+			ChatID  string `json:"chat_id"`
+			History []struct {
+				ChatIndex int    `json:"chat_index"`
+				Message   string `json:"message"`
+				Type      string `json:"type"`
+				Filename  string `json:"filename,omitempty"`
+			} `json:"history"`
+			VisitorID string `json:"visitor_id"`
+		} `json:"value"`
+	} `json:"chat_event"`
+}
+
+// RedactChatComment redacts a chat comment in a ticket.
+//
+// ref: https://developer.zendesk.com/api-reference/ticketing/tickets/ticket_comments/#redact-chat-comment
+func (z *Client) RedactChatComment(ctx context.Context, req *RedactChatCommentRequest) (*RedactChatResponse, error) {
+	result := &RedactChatResponse{}
+	resp, err := z.put(ctx, fmt.Sprintf("/chat_redactions/%d.json", req.TicketID), req)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(resp, &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+// RedactChatCommentAttachment redacts an attachments from chat comments in a ticket.
+// NOTE: for chats, zendesk allows only one attachment per message
+//
+// ref: https://developer.zendesk.com/api-reference/ticketing/tickets/ticket_comments/#redact-chat-comment-attachment
+func (z *Client) RedactChatCommentAttachment(ctx context.Context, req *RedactChatCommentAttachmentRequest) (*RedactChatResponse, error) {
+	result := &RedactChatResponse{}
+	resp, err := z.put(ctx, fmt.Sprintf("/chat_file_redactions/%d.json", req.TicketID), req)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(resp, &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
---
+++ zendesk/chat_test.go
@@ -0,0 +1,47 @@
+package zendesk
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestRedactChatComment(t *testing.T) {
+	mockAPI := newMockAPI(http.MethodPut, "redact_chat_comment.json")
+	client := newTestClient(mockAPI)
+	defer mockAPI.Close()
+
+	req := &RedactChatCommentRequest{
+		TicketID:  123,
+		MessageID: "abcd-1234-efgh-5678",
+		Text:      "Redacted by Go-Zendesk",
+	}
+
+	resp, err := client.RedactChatComment(ctx, req)
+	if err != nil {
+		t.Fatalf("Failed to redact chat comment: %s", err)
+	}
+
+	if resp.ChatEvent.ID != 1932802680168 {
+		t.Fatalf("Expected chat event ID to be 1932802680168, got %d", resp.ChatEvent.ID)
+	}
+}
+
+func TestRedactChatCommentAttachment(t *testing.T) {
+	mockAPI := newMockAPI(http.MethodPut, "redact_chat_comment_attachment.json")
+	client := newTestClient(mockAPI)
+	defer mockAPI.Close()
+
+	req := &RedactChatCommentAttachmentRequest{
+		TicketID:   123,
+		MessageIDs: []string{"abcd-1234-efgh-5678"},
+	}
+
+	resp, err := client.RedactChatCommentAttachment(ctx, req)
+	if err != nil {
+		t.Fatalf("Failed to redact chat comment attachment: %s", err)
+	}
+
+	if resp.ChatEvent.ID != 1932802680168 {
+		t.Fatalf("Expected chat event ID to be 1932802680168, got %d", resp.ChatEvent.ID)
+	}
+}
